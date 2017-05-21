8796656e085066782086bab12c867d66f1912d37
Phillip Freeman <paf31@cantab.net>
2017-05-21 16:36:08 -0700

    JSON example

--- test/Main.purs
+++ test/Main.purs
@@ -4,13 +4,29 @@ import Prelude
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Class (liftEff)
 import Control.Monad.Eff.Console (CONSOLE, log)
+import Data.Foreign.Class (class Decode, class Encode)
+import Data.Foreign.Generic (defaultOptions, genericDecode, genericEncode)
+import Data.Foreign.Generic.Types (Options)
 import Data.Generic.Rep (class Generic)
 import Data.Maybe (Maybe(..))
 import Node.HTTP (HTTP)
-import QuickServe (class Servable, GET, POST, RequestBody(RequestBody), genericServeWith, quickServe)
+import QuickServe (class Servable, GET, JSON(..), POST, RequestBody(..), genericServeWith, quickServe)
+
+newtype Message = Message { message :: String }
+
+derive instance genericMessage :: Generic Message _
+
+jsonOpts :: Options
+jsonOpts = defaultOptions { unwrapSingleConstructors = true }
+
+instance decodeMessage :: Decode Message where
+  decode = genericDecode jsonOpts
+
+instance encodeMessage :: Encode Message where
+  encode = genericEncode jsonOpts
 
 newtype Routes eff = Routes
-  { echo :: RequestBody String -> POST (console :: CONSOLE | eff) String
+  { echo :: RequestBody (JSON Message) -> POST (console :: CONSOLE | eff) (JSON Message)
   , hello :: GET (console :: CONSOLE | eff) String
   }
 
@@ -21,9 +37,9 @@ instance servableRoutes :: Servable (console :: CONSOLE | eff) (Routes eff) wher
 
 server :: forall eff. Routes eff
 server = Routes
-  { echo: \(RequestBody s) -> do
-      liftEff (log s)
-      pure s
+  { echo: \(RequestBody (JSON (Message { message }))) -> do
+      liftEff (log message)
+      pure (JSON (Message { message }))
   , hello: pure "Hello, World!"
   }
 
